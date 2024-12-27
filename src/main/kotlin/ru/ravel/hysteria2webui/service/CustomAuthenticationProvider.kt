7e7b984135f777aae3f48735fd612d827c284376
petr <petyalom@yandex.ru>
2024-12-27 23:20:32 +0300

    fix password encoding

--- src/main/kotlin/ru/ravel/hysteria2webui/service/CustomAuthenticationProvider.kt
+++ src/main/kotlin/ru/ravel/hysteria2webui/service/CustomAuthenticationProvider.kt
@@ -8,6 +8,7 @@ import org.springframework.security.core.AuthenticationException
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.User
 import org.springframework.stereotype.Component
+import java.util.Base64
 
 
 @Component
@@ -18,7 +19,9 @@ class CustomAuthenticationProvider(
 
 	@Throws(AuthenticationException::class)
 	override fun authenticate(authentication: Authentication): Authentication? {
-		val password = authentication.credentials.toString()
+		val base64password = authentication.credentials.toString()
+		val decodedBytes = Base64.getDecoder().decode(base64password)
+		val password = String(decodedBytes, Charsets.UTF_8)
 		return if (password == this.password) {
 			val authorities: List<GrantedAuthority> = emptyList()
 			val userDetails = User("admin", password, authorities)
